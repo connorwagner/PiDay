1be787d67ede66227d35d235320972b1f0a962c6
benfritzeen <bfritzeen@gmail.com>
2017-12-11 12:45:58 -0500

    Completed Connect4 & Othello, needs to be adapted for PiDay

---
+++ ConnectFour.py
@@ -0,0 +1,197 @@
+## Written by Ben Fritzeen
+## 12/10/2017
+## Connect-Four game to be used in PiDay
+
+
+class ConnectFour:
+    def __init__(self):
+
+        self.gameBoard = []
+        self.spotsUsed = 0
+        self.winner = -1
+        self.recentState = -1
+        self.recentCol = 0
+
+        for i in range(6):
+            self.gameBoard.append([0, 0, 0, 0, 0, 0, 0])
+
+    def getSpotState(self, row, col):
+        return self.gameBoard[row][col]
+
+    def setSpotState(self, col, state):
+        for row in range(5, -1, -1):
+            if self.gameBoard[row][col] == 0:
+                self.gameBoard[row][col] = state
+                self.spotsUsed += 1
+                return
+
+    def getNumSpotsLeft(self):
+        return 42 - self.spotsUsed
+
+    def whoseTurn(self):
+        if self.getNumSpotsLeft() % 2 == 0:
+            return 1
+        else:
+            return 2
+
+    def isValidMove(self, string):
+        if string.isdigit() and len(string) == 1 and int(string[0]) >= 0 and int(string[0]) <= 6:
+            return True
+        else:
+            return False
+
+    def isWinner(self, col, state):
+        row = 0
+        for lastRow in range(6):
+            if self.gameBoard[lastRow][col] != 0:
+                row = lastRow
+                break
+
+        if self.checkForFour(row, col, state):
+            self.winner = state
+            return True
+
+    def checkForFour(self, row, col, state):
+        if self.checkRightLeft(row, col, state):
+            return True
+        elif self.checkUpDown(row, col, state):
+            return True
+        elif self.checkDownLeftUpRight(row, col, state):
+            return True
+        elif self.checkDownRightUpLeft(row, col, state):
+            return True
+        else:
+            return False
+
+    def checkDownRightUpLeft(self, row, col, state):
+        connectCtr = 1
+        ctr = 1
+        while (row + ctr != 6 and col + ctr != 7):
+            if connectCtr == 4:
+                return True
+            if self.gameBoard[row + ctr][col + ctr] == state:
+                connectCtr += 1
+                ctr += 1
+            else:
+                break
+
+        ctr = 1
+        while (row - ctr != -1 and col - ctr != -1):
+            if connectCtr == 4:
+                return True
+            if self.gameBoard[row - ctr][col - ctr] == state:
+                connectCtr += 1
+                ctr += 1
+            else:
+                break
+
+    def checkDownLeftUpRight(self, row, col, state):
+        connectCtr = 1
+        ctr = 1
+        while (row + ctr != 6 and col - ctr != -1):
+            if connectCtr == 4:
+                return True
+            if self.gameBoard[row + ctr][col - ctr] == state:
+                connectCtr += 1
+                ctr += 1
+            else:
+                break
+
+        ctr = 1
+        while (row - ctr != -1 and col + ctr != 7):
+            if connectCtr == 4:
+                return True
+            if self.gameBoard[row - ctr][col + ctr] == state:
+                connectCtr += 1
+                ctr += 1
+            else:
+                break
+
+    def checkUpDown(self, row, col, state):
+        connectCtr = 1
+        for downRow in range(row + 1, 6):
+            if connectCtr == 4:
+                return True
+            if self.gameBoard[downRow][col] == state:
+                connectCtr += 1
+            else:
+                break
+        for upRow in range(row - 1, -1, -1):
+            if connectCtr == 4:
+                return True
+            if self.gameBoard[upRow][col] == state:
+                connectCtr += 1
+            else:
+                break
+        return False
+
+    def checkRightLeft(self, row, col, state):
+        connectCtr = 1
+        for rightCol in range(col + 1, 7):
+            if connectCtr == 4:
+                return True
+            if self.gameBoard[row][rightCol] == state:
+                connectCtr += 1
+            else:
+                break
+        for leftCol in range(col - 1, -1, -1):
+            if connectCtr == 4:
+                return True
+            if self.gameBoard[row][leftCol] == state:
+                connectCtr += 1
+            else:
+                break
+        return False
+
+    def playerMove(self):
+        if self.whoseTurn() == 1:
+            move = input("Player One's move: ")
+            while (not self.isValidMove(move)):
+                print("Invalid move. Try again!")
+                move = input("Player One's move: ")
+            col = int(move[0])
+            while (self.getSpotState(0, col) != 0):
+                print("Column is already full! Try again!")
+                move = input("Player One's move: ")
+                while (not self.isValidMove(move)):
+                    print("Invalid move. Try again!")
+                    move = input("Player One's move: ")
+                col = int(move[0])
+            self.setSpotState(col, 1)
+            self.setMostRecent(col, 1)
+        else:
+            move = input("Player Two's move: ")
+            while (not self.isValidMove(move)):
+                print("Invalid move. Try again!")
+                move = input("Player Two's move: ")
+            col = int(move[0])
+            while (self.getSpotState(0, col) != 0):
+                print("Column is already full! Try again!")
+                move = input("Player Two's move: ")
+                col = int(move[0])
+            self.setSpotState(col, 2)
+            self.setMostRecent(col, 2)
+
+    def setMostRecent(self, col, state):
+        self.recentState = state
+        self.recentCol = col
+
+    def displayBoard(self):
+        for i in range(6):
+            print(self.gameBoard[i])
+
+    def gameControl(self):
+        print("Enter your moves in the format of col. Ex: '2' is column 2")
+        while (not self.isWinner(self.recentCol, self.recentState)):
+            if (self.getNumSpotsLeft() == 0):
+                print("Draw!!")
+                return
+            self.playerMove()
+            self.displayBoard()
+        print("Player " + str(self.winner) + " Wins!!")
+
+
+connectFour = ConnectFour()
+connectFour.gameControl()
+
+
---
+++ Othello.py
@@ -0,0 +1,241 @@
+## Written by Ben Fritzeen
+## 12/10/2017
+## Othello game to be used in PiDay
+
+class Othello:
+    def __init__(self):
+        self.gameBoard = []
+        self.oneCtr = 0
+        self.twoCtr = 0
+
+        for i in range(8):
+            self.gameBoard.append([0, 0, 0, 0, 0, 0, 0, 0])
+
+    def getSpotState(self, row, col):
+        return self.gameBoard[row][col]
+
+    def setSpotState(self, row, col, state):
+        self.gameBoard[row][col] = state
+        if state == 1:
+            self.oneCtr += 1
+        elif state == 2:
+            self.twoCtr += 1
+
+    def getNumSpotsLeft(self):
+        return 64 - (self.twoCtr + self.oneCtr)
+
+    def isWinner(self):
+        return 0 == self.getNumSpotsLeft()
+
+    def whoWon(self):
+        if self.isWinner:
+            if self.oneCtr > self.twoCtr:
+                return "Player One won!!"
+            elif self.twoCtr > self.oneCtr:
+                return "Player Two Won!!"
+            else:
+                return "It's a tie!!"
+        else:
+            return "No winner yet"
+
+    def whoseTurn(self):
+        if self.getNumSpotsLeft() % 2 == 0:
+            return 1
+        else:
+            return 2
+
+    def playerMove(self):
+        if self.whoseTurn() == 1:
+            move = input("Player One's move: ")
+            while (not self.isValidMove(move)):
+                print("Invalid move. Try again!")
+                move = input("Player One's move: ")
+            row = int(move[0])
+            col = int(move[1])
+            while (self.getSpotState(row, col) != 0):
+                print("Spot already taken. Try again!")
+                move = input("Player One's move: ")
+                while (not self.isValidMove(move)):
+                    print("Invalid move. Try again!")
+                    move = input("Player One's move: ")
+                row = int(move[0])
+                col = int(move[1])
+            self.checkForSwaps(row, col, 1)
+            self.setSpotState(row, col, 1)
+        else:
+            move = input("Player Two's move: ")
+            while (not self.isValidMove(move)):
+                print("Invalid move. Try again!")
+                move = input("Player Two's move: ")
+            row = int(move[0])
+            col = int(move[1])
+            while (self.getSpotState(row, col) != 0):
+                print("Spot already taken. Try again!")
+                move = input("Player Two's move: ")
+                while (not self.isValidMove(move)):
+                    print("Invalid move. Try again!")
+                    move = input("Player Two's move: ")
+                row = int(move[0])
+                col = int(move[1])
+            self.checkForSwaps(row, col, 2)
+            self.setSpotState(row, col, 2)
+
+    def checkForSwaps(self, row, col, state):
+        self.checkDownForSwaps(row, col, state)
+        self.checkUpForSwaps(row, col, state)
+        self.checkLeftForSwaps(row, col, state)
+        self.checkRightForSwaps(row, col, state)
+        self.checkDownRightForSwaps(row, col, state)
+        self.checkDownLeftForSwaps(row, col, state)
+        self.checkUpLeftForSwaps(row, col, state)
+        self.checkUpRightForSwaps(row, col, state)
+
+    def checkUpRightForSwaps(self, row, col, state):
+        upRightList = []
+        ctr = 1
+        while (row + ctr != -1 and col + ctr != 8):
+            if self.gameBoard[row - ctr][col + ctr] == state:
+                if len(upRightList) != 0:
+                    for spot in upRightList:
+                        self.gameBoard[spot[0]][spot[1]] = state
+                    return
+                else:
+                    return
+            elif self.gameBoard[row - ctr][col + ctr] == 0:
+                return
+            else:
+                upRightList.append([row - ctr, col + ctr])
+                ctr += 1
+
+    def checkDownLeftForSwaps(self, row, col, state):
+        downLeftList = []
+        ctr = 1
+        while (row + ctr != 8 and col + ctr != -1):
+            if self.gameBoard[row + ctr][col - ctr] == state:
+                if len(downLeftList) != 0:
+                    for spot in downLeftList:
+                        self.gameBoard[spot[0]][spot[1]] = state
+                    return
+                else:
+                    return
+            elif self.gameBoard[row + ctr][col - ctr] == 0:
+                return
+            else:
+                downLeftList.append([row + ctr, col - ctr])
+                ctr += 1
+
+    def checkUpLeftForSwaps(self, row, col, state):
+        upLeftList = []
+        ctr = 1
+        while (row + ctr != -1 and col + ctr != -1):
+            if self.gameBoard[row - ctr][col - ctr] == state:
+                if len(upLeftList) != 0:
+                    for spot in upLeftList:
+                        self.gameBoard[spot[0]][spot[1]] = state
+                    return
+                else:
+                    return
+            elif self.gameBoard[row - ctr][col - ctr] == 0:
+                return
+            else:
+                upLeftList.append([row - ctr, col - ctr])
+                ctr += 1
+
+    def checkDownRightForSwaps(self, row, col, state):
+        downRightList = []
+        ctr = 1
+        while (row + ctr != 8 and col + ctr != 8):
+            if self.gameBoard[row + ctr][col + ctr] == state:
+                if len(downRightList) != 0:
+                    for spot in downRightList:
+                        self.gameBoard[spot[0]][spot[1]] = state
+                    return
+                else:
+                    return
+            elif self.gameBoard[row + ctr][col + ctr] == 0:
+                return
+            else:
+                downRightList.append([row + ctr, col + ctr])
+                ctr += 1
+
+    def checkDownForSwaps(self, row, col, state):
+        downList = []
+        for i in range(row + 1, 8):
+            if self.gameBoard[i][col] == state:
+                if len(downList) != 0:
+                    for spot in downList:
+                        self.gameBoard[spot[0]][spot[1]] = state
+                    return
+                else:
+                    return
+            elif self.gameBoard[i][col] == 0:
+                return
+            else:
+                downList.append([i, col])
+
+    def checkUpForSwaps(self, row, col, state):
+        upList = []
+        for i in range(row - 1, -1, -1):
+            if self.gameBoard[i][col] == state:
+                if len(upList) != 0:
+                    for spot in upList:
+                        self.gameBoard[spot[0]][spot[1]] = state
+                    return
+                else:
+                    return
+            elif self.gameBoard[i][col] == 0:
+                return
+            else:
+                upList.append([i, col])
+
+    def checkLeftForSwaps(self, row, col, state):
+        leftList = []
+        for i in range(col - 1, -1, -1):
+            if self.gameBoard[row][i] == state:
+                if len(leftList) != 0:
+                    for spot in leftList:
+                        self.gameBoard[spot[0]][spot[1]] = state
+                    return
+                else:
+                    return
+            elif self.gameBoard[row][i] == 0:
+                return
+            else:
+                leftList.append([row, i])
+
+    def checkRightForSwaps(self, row, col, state):
+        rightList = []
+        for i in range(col + 1, 8):
+            if self.gameBoard[row][i] == state:
+                if len(rightList) != 0:
+                    for spot in rightList:
+                        self.gameBoard[spot[0]][spot[1]] = state
+                    return
+                else:
+                    return
+            elif self.gameBoard[row][i] == 0:
+                return
+            else:
+                rightList.append([row, i])
+
+    def isValidMove(self, string):
+        if string.isdigit():
+            return True
+        else:
+            return False
+
+    def displayBoard(self):
+        for i in range(8):
+            print(self.gameBoard[i])
+
+    def gameControl(self):
+        print("Enter your moves in the format of rowCol. Ex: '12' is row 1, column 2")
+        while (not self.isWinner()):
+            self.playerMove()
+            self.displayBoard()
+        self.whoWon()
+
+
+othello = Othello()
+othello.gameControl()
+
